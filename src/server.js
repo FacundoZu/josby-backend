4a1de58dcfbea252f8556b6e4ac3882f5ce70398
Nicolas Nuñez <nuneznicolas582@gmail.com>
2025-11-13 12:07:31 -0300

    Add service routes

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import authRoutes from './routes/authRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import categoryRoutes from './routes/categoryRoutes.js';
 import skillRoutes from './routes/skillRoutes.js';
+import serviceRoutes from './routes/serviceRoutes.js'
 
 dotenv.config();
 const app = express();
@@ -26,6 +27,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/category', categoryRoutes)
 app.use('/api/skill', skillRoutes)
+app.use('/api/service', serviceRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
